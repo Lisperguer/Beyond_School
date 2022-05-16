4f65c9c4ecdfa03bd8caf6420783482b7916d61b
Alan Perkul <alanperkul@gmail.com>
2022-05-16 19:04:55 -0300

    controller creados con dudas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  get 'sections/show'
+  get 'courses/index'
+  get 'courses/show'
   devise_for :users
   root to: 'pages#home'
+  resources :courses, only: [:index, :show] do
+    resources :sections, only: [:show] do
+      resources :quizzes, only: [:show]
+    end
+  end
+  resources :quizzes do
+    resources :quiz_details
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
