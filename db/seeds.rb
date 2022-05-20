4dd979abbe7f6e3139c30352881a9c417708b651
Florencia Lema <flor.lema222@gmail.com>
2022-05-19 23:05:40 -0300

    cloudinary working - home and index con cloudinary e iteracion

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,28 @@ User.destroy_all
 User.create(email: "felipe.lisperguer@gmail.com", first_name: "Felipe", last_name: "Lisperguer", address: "Siempreviva 123", age: "27", school: "Santa Maria", password: "123456", password_confirmation: "123456")
 User.create(email: "flo.lema222@gmail.com", first_name: "Florencia", last_name: "Lema", address: "Calle Falsa 123", age: "58", school: "Santa Catalina", password: "123456", password_confirmation: "123456")
 
-Course.create(titulo: "Educacion Sexual", duracion: 10, detalle: "La educación sexual es muy importante, ya que esta proporciona la información y las herramientas necesarias para disfrutar del sexo de manera responsable y saludable", photo: cl_image_tag("sexualidad_qdiutd",
-  width: 400, height: 300, crop: :fill))
+curso0 = Course.create(titulo: "Educacion Sexual", duracion: 10, detalle: "La educación sexual es muy importante, ya que esta proporciona la información y las herramientas necesarias para disfrutar del sexo de manera responsable y saludable")
 curso1 = Course.create(titulo: "Educacion Financiera", duracion: 20, detalle: "La educación financiera es el proceso mediante el cual los individuos adquieren una mejor comprensión de los conceptos y productos financieros, desarrollan las habilidades necesarias para tomar decisiones  informadas, evaluar riesgos y oportunidades financieras")
-Course.create(titulo: "Nutricion", duracion: 15, detalle: "La educación nutricional se refiere a la difusión de información útil acerca de los alimentos y sus nutrientes, pero su función no se reduce a ello, sino también a ofrecer las herramientas necesarias para mejorar los hábitos de alimentación y en consecuencia los procesos de nutrición.")
-Course.create(titulo: "Introduccion a las nuevas tecnologias", duracion: 35, detalle: "Las 'nuevas tecnologías' son aquellas corrientes tecnológicas que están marcando un cambio en el modelo industrial y social en la economía global. En otras palabras, consiste en un conjunto de tecnologías de temprana creación o desarrollo que forman parte de la denominada 'revolución digital'.")
-Course.create(titulo: "Matematica", duracion: 50, detalle: "La mayoría de las personas pensamos que las matemáticas sólo se tratan de números para realizar operaciones como sumas, restas, multiplicaciones, fracciones, etc. Sin embargo, sus funciones están directamente relacionadas con nuestras actividades diarias.")
+curso2 = Course.create(titulo: "Nutricion", duracion: 15, detalle: "La educación nutricional se refiere a la difusión de información útil acerca de los alimentos y sus nutrientes, pero su función no se reduce a ello, sino también a ofrecer las herramientas necesarias para mejorar los hábitos de alimentación y en consecuencia los procesos de nutrición.")
+curso3 = Course.create(titulo: "Introduccion a las nuevas tecnologias", duracion: 35, detalle: "Las 'nuevas tecnologías' son aquellas corrientes tecnológicas que están marcando un cambio en el modelo industrial y social en la economía global. En otras palabras, consiste en un conjunto de tecnologías de temprana creación o desarrollo que forman parte de la denominada 'revolución digital'.")
+curso4 = Course.create(titulo: "Matematica", duracion: 50, detalle: "La mayoría de las personas pensamos que las matemáticas sólo se tratan de números para realizar operaciones como sumas, restas, multiplicaciones, fracciones, etc. Sin embargo, sus funciones están directamente relacionadas con nuestras actividades diarias.")
+
+url0 = "https://res.cloudinary.com/dydeg1evr/image/upload/v1652999187/development/sexualidad_qdiutd.jpg"
+curso0.photo.attach(io: URI.open(url0), filename: "sexualidad_qdiutd.jpg", content_type: "image/jpeg")
+curso0.save
+
+url1 = "https://res.cloudinary.com/dydeg1evr/image/upload/v1652999186/development/Educacion-financiera_a6w6bg.jpg"
+curso1.photo.attach(io: URI.open(url1), filename: "Educacion-financiera_a6w6bg.jpg", content_type: "image/jpeg")
+curso1.save
+url2 = "https://res.cloudinary.com/dydeg1evr/image/upload/v1653005367/development/nutricion_kxxb2m.jpg"
+curso2.photo.attach(io: URI.open(url2), filename: "nutricion_kxxb2m.jpg", content_type: "image/jpeg")
+curso2.save
+url3 = "https://res.cloudinary.com/dydeg1evr/image/upload/v1652999187/development/tecnologias_bvvdlr.webp"
+curso3.photo.attach(io: URI.open(url3), filename: "tecnologias_bvvdlr.webp", content_type: "image/jpeg")
+curso3.save
+url4 = "https://res.cloudinary.com/dydeg1evr/image/upload/v1652999186/development/mathematics_ovoibd.jpg"
+curso4.photo.attach(io: URI.open(url4), filename: "mathematics_ovoibd.jpg", content_type: "image/jpeg")
+curso4.save
 
 url_section_1 = URI.open("https://www.youtube.com/watch?v=F6rruydac9I")
 url_section_2 = URI.open("https://www.youtube.com/watch?v=ALlSherR2ss")
