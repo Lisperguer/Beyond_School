cc036f07f41c96aba708d4330ae80beabff50082
Florencia Lema <flor.lema222@gmail.com>
2022-05-24 18:51:13 -0300

    reviews rusticas

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
 
-  resources :courses, only: [:index, :show, :my_course]
+  resources :courses, only: [:index, :show, :my_course] do
+    resources :reviews
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sections, only: [:show]
   resources :quizzes
