506df2414847f7f3cf9c026aaeabd92d45892588
Felipe Lisperguer <felipe.lisperguer.h@gmail.com>
2022-05-24 17:52:50 -0400

    Se volvió al quizz anterior

--- app/controllers/quiz_statuses_controller.rb
+++ app/controllers/quiz_statuses_controller.rb
@@ -1,23 +1,49 @@
 class QuizStatusesController < ApplicationController
-  # def create
-  #   if params[:pregunta_a]["respuesta_1"] == "1" && params[:pregunta_b]["respuesta_2"] == "1" && params[:pregunta_c]["respuesta_3"] == "1"
-  #     g = Section.where(id: params["current_section"]["r"].to_i + 1).first
-  #     if g
-  #     redirect_to section_path(g)
-  #     else
-  #     redirect_to root_path
-  #     end
-  #   else
-  #     # si desaprobe =>
-  #     redirect_to root_path
-  #   end
-  # end
 
-  def quizzquestions
-    
-    respond_to do |format|
-      format.js
+  function sweetalertclick() {
 
+swal({
+    title: 'Request a DDS',
+    text: "Please log in to DDS website to request a pick up, my code is: xxxx. By pressing ok you will link to google.com",
+    showCancelButton: true,
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: "Ok",
+  }).then((result) => {
+  console.log(result)
+    if (result) {
+      window.location.href = "google.com";
+    }
+  });
+  
+  def create
+    if params[:pregunta_a]["respuesta_1"] == "1" && params[:pregunta_b]["respuesta_2"] == "1" && params[:pregunta_c]["respuesta_3"] == "1"
+      g = Section.where(id: params["current_section"]["r"].to_i + 1).first
+      if g
+        #Hacer un contador con la cantidad de respuestas de buena( contador i =0)
+        #Hacer un if para ver si el contador es mayor a cierto puntaje
+          #Si es mayor lanzar un alert y que el botón de ese alert tenga el redirect
+          #Y si no, lanzar alert nuevamente y que el bóton de ese alert te lleve al mismo modulo
+          sweetalertclick
+          flash[:notice] = 'Has pasado el módulo, felicidades!'
+          redirect_to section_path(g)
+      else
+        flash[:notice] = 'No has pasado el modulo, vuelve a intentarlo!'
+
+        redirect_to root_path
+      end
+    else
+      # si desaprobe =>
+      flash[:notice] = 'No has pasado el modulo, vuelve a intentarlo!'
+
+      redirect_to root_path
     end
   end
+
+  # def quizzquestions
+    
+  #   respond_to do |format|
+  #     format.js
+
+  #   end
+  # end
 end
