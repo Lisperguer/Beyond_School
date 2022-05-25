89ee7545721fda1f1f5d7aecf115a3faacdf7ec6
Denny Rodas <denny.rh182@gmail.com>
2022-05-25 12:39:23 -0400

    cerrar seision boton

--- app/controllers/quiz_statuses_controller.rb
+++ app/controllers/quiz_statuses_controller.rb
@@ -1,20 +1,4 @@
 class QuizStatusesController < ApplicationController
-
-  function sweetalertclick() {
-
-swal({
-    title: 'Request a DDS',
-    text: "Please log in to DDS website to request a pick up, my code is: xxxx. By pressing ok you will link to google.com",
-    showCancelButton: true,
-    confirmButtonColor: "#DD6B55",
-    confirmButtonText: "Ok",
-  }).then((result) => {
-  console.log(result)
-    if (result) {
-      window.location.href = "google.com";
-    }
-  });
-  
   def create
     if params[:pregunta_a]["respuesta_1"] == "1" && params[:pregunta_b]["respuesta_2"] == "1" && params[:pregunta_c]["respuesta_3"] == "1"
       g = Section.where(id: params["current_section"]["r"].to_i + 1).first
@@ -25,7 +9,7 @@ swal({
         #Hacer un if para ver si el contador es mayor a cierto puntaje
           #Si es mayor lanzar un alert y que el botón de ese alert tenga el redirect
           #Y si no, lanzar alert nuevamente y que el bóton de ese alert te lleve al mismo modulo
-          sweetalertclick
+          # sweetalertclick
           flash[:notice] = 'Has pasado el módulo, felicidades!'
           redirect_to section_path(g)
       else
@@ -40,12 +24,4 @@ swal({
       redirect_to section_path(h)
     end
   end
-
-  # def quizzquestions
-    
-  #   respond_to do |format|
-  #     format.js
-
-  #   end
-  # end
 end
